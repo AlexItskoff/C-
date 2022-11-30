31ec6ce17eb370da73d458d5e160ed7111f68304
Alex <platoon880@gmail.com>
2022-12-01 01:11:06 +0300

    e5_01

---
+++ Example005_if_else/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if (username.ToLower()  ==  "sasha")
+
+{
+
+Console.WriteLine("Hi, sasha");
+
+
+}
+
+else
+{
+    
+Console.Write( "Hi, there");
+Console.WriteLine( username);
+
+
+
+}
+
+
